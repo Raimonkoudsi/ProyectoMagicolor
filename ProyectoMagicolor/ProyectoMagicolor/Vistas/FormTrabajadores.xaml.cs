04fcc67adf2c5465434eff3866910caf37ddec7c
Jose Pereira <wallbangstudios4.9@gmail.com>
2020-12-16 23:44:25 -0400

    Form Crear

--- ProyectoMagicolor/ProyectoMagicolor/Vistas/FormTrabajadores.xaml.cs
+++ ProyectoMagicolor/ProyectoMagicolor/Vistas/FormTrabajadores.xaml.cs
@@ -23,5 +23,10 @@ namespace ProyectoMagicolor.Vistas
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
