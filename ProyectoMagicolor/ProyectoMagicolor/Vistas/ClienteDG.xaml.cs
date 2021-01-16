5f2307efe8f5f47e838998dcf00902d8be0e78c5
Jose Pereira <wallbangstudios4.9@gmail.com>
2021-01-16 15:58:03 -0400

    Agregando Form Cliente

---
+++ ProyectoMagicolor/ProyectoMagicolor/Vistas/ClienteDG.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Datos;
+using Logica;
+
+namespace ProyectoMagicolor.Vistas
+{
+    /// <summary>
+    /// Interaction logic for FTrabajadores.xaml
+    /// </summary>
+    public partial class ClienteaDG : Page
+    {
+
+        public LCategoria Metodos = new LCategoria();
+
+        public ClienteaDG()
+        {
+            InitializeComponent();
+        }
+        
+
+        public void Refresh(string search)
+        {
+
+            List<DCategoria> items = Metodos.Mostrar(search);
+
+
+            dgOperaciones.ItemsSource = items;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //contentsp.Children.Clear();
+
+            Refresh(txtBuscar.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //if (dgOperaciones.SelectedItems.Count > 0)
+            //{
+            //    for(int i = 0; i < dgOperaciones.SelectedItems.Count; i++)
+            //    {
+            //        MessageBox.Show(((TablaTrabajadores)dgOperaciones.SelectedItems[i]).Nombre);
+            //    }
+            //}
+            //else
+            //    MessageBox.Show("no hay");
+            CategoriaFrm frmTrab = new CategoriaFrm();
+            bool Resp = frmTrab.ShowDialog() ?? false;
+            Refresh(txtBuscar.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int id = (int)((Button)sender).CommandParameter;
+            var response = Metodos.Encontrar(id);
+
+            CategoriaFrm frm = new CategoriaFrm();
+            frm.Type = TypeForm.Update;
+            frm.DataFill = response[0];
+            bool Resp = frm.ShowDialog() ?? false;
+            Refresh(txtBuscar.Text);
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            Refresh(txtBuscar.Text);
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult Resp = MessageBox.Show("¿Seguro que quieres eliminrar este item?", "Magicolor", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (Resp != MessageBoxResult.Yes)
+                return;
+            int id = (int)((Button)sender).CommandParameter;
+            DCategoria item = new DCategoria()
+            {
+                idCategoria = id
+            };
+            Metodos.Eliminar(item);
+            Refresh(txtBuscar.Text);
+        }
+
+        private void txtBuscar_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(txtBuscar.Text == "")
+            {
+               txtBucarPlaceH.Text = "";
+            }
+            
+        }
+
+        private void txtBuscar_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(txtBuscar.Text == "")
+            {
+                txtBucarPlaceH.Text = "Buscar...";
+            }
+            
+        }
+
+        private void txtVer_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (int)((Button)sender).CommandParameter;
+            var response = Metodos.Encontrar(id);
+
+            CategoriaFrm frmTrab = new CategoriaFrm();
+            frmTrab.Type = TypeForm.Read;
+            frmTrab.DataFill = response[0];
+            bool Resp = frmTrab.ShowDialog() ?? false;
+            Refresh(txtBuscar.Text);
+
+            //MessageBox.Show(response[0].fechaNacimiento.ToString());
+        }
+    }
+
+}
---
+++ ProyectoMagicolor/ProyectoMagicolor/Vistas/ClienteFrm.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Datos;
+using Logica;
+
+namespace ProyectoMagicolor.Vistas
+{
+    /// <summary>
+    /// Interaction logic for FormTrabajadores.xaml
+    /// </summary>
+    public partial class ClienteFrm : Window
+    {
+
+
+        public ClienteaDG()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        public TypeForm Type;
+        public DCategoria DataFill;
+
+        public DCategoria UForm;
+
+        public LCategoria Metodos = new LCategoria();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Type == TypeForm.Update)
+                Update();
+            else
+                Create();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(Type == TypeForm.Read)
+            {
+                txtTitulo.Text = "Leer Categoria";
+                fillForm(DataFill);
+                SetEnable(false);
+                btnEnviar.Visibility = Visibility.Collapsed;
+            }
+            else if(Type == TypeForm.Update)
+            {
+                txtTitulo.Text = "Editar Categoria";
+                fillForm(DataFill);
+            }
+        }
+
+       
+
+        void fillData()
+        {
+            if (Validate())
+            {
+                UForm = null;
+                return;
+            }
+
+            string nombre = txtNombre.txt.Text;
+            string descripcion = txtDescripcion.Text;
+
+            UForm = new DCategoria(0, nombre, descripcion);
+        }
+
+        void Create()
+        {
+            fillData();
+            if (UForm == null)
+                return;
+            string response = Metodos.Insertar(UForm);
+            MessageBox.Show(response);
+            if (response == "OK")
+            {
+                this.DialogResult = true;
+                this.Close();
+            }
+
+        }
+
+        void Update()
+        {
+            fillData();
+            if (UForm == null)
+                return;
+            UForm.idCategoria = DataFill.idCategoria;
+            string response = Metodos.Editar(UForm);
+            MessageBox.Show(response);
+            if(response == "OK")
+            {
+                this.DialogResult = true;
+                this.Close();
+            }
+        }
+
+        private void PlaceDescripcion_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(txtDescripcion.Text == "")
+            {
+                PlaceDescripcion.Text = "";
+            }
+        }
+
+        private void PlaceDescripcion_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtDescripcion.Text == "")
+            {
+                PlaceDescripcion.Text = "Descripción";
+            }
+        }
+
+        void SetEnable(bool Enable)
+        {
+            txtNombre.IsEnabled = Enable;
+            txtDescripcion.IsEnabled = Enable;
+        }
+        void fillForm(DCategoria Data)
+        {
+            if(Data != null)
+            {
+                txtNombre.SetText(Data.nombre);
+                txtDescripcion.Text = Data.descripcion;
+                PlaceDescripcion.Text = "";
+            }
+        }
+        #region Validation
+        bool Validate()
+        {
+            if (!txtNombre.Changed)
+            {
+                MessageBox.Show("Debes llenar el campo Nombre!", "Magicolor", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtNombre.txt.Focus();
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
+
+
+    }
+}
